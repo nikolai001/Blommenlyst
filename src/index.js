044d10d2fced501594db35027849f7c28f908ea1
nikolairaahauge <nira36696@edu.ucl.dk>
2023-02-07 20:21:05 +0100

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+let players = [
+    {firstName: 'Nikolai', lastName: 'Raahauge', playerHCP: 52},
+    {firstName: 'Ronni', lastName: 'Raahauge', playerHCP: 14}
+]
+
+function MyComponent(props) {
+    return <ul>{players.map(player => <li>{player.firstName} {player.lastName} - {player.playerHCP}</li>)}</ul>
+}
+
+function Main () {
+    return (
+        <main>
+            <MyComponent/>
+            <h1>Hello there</h1>
+            <h2>General Kenobi</h2>
+            <p>How you doin</p>
+        </main>
+    );
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<Main />);
